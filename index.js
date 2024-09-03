fc6f17af33315fe40015e94db0ace8209043779c
itszisa <167381636+itszisa@users.noreply.github.com>
2024-09-03 23:07:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("O meu primeiro nome e Isabelle Cristyne Da Silva Da Cruz , tenho 19 anos Nascida e criada em Curitiba PR Brasil , tenho 1.55 de altura:");
+
+const primeiroNome = "Isabelle"; 
+console.log(primeiroNome);
+const idade = 19;
+const cidadeNatal = "Curitiba";
+const altura = 1.55;
+
+const segundoNome = "Cristyne";
+console.log(segundoNome);
+console.log(idade);
+console.log(cidadeNatal);
+console.log(altura);
